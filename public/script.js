e4ce118ecbe4470b69306ea9cf1949995adf5935
pratapdeep <deeptiranjansethi46@gmail.com>
2020-08-29 16:58:33 +0530

    chat div

--- public/script.js
+++ public/script.js
@@ -3,12 +3,13 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '443'
+  port: '3030'
 })
 let myVideoStream;
 const myVideo = document.createElement('video')
 myVideo.muted = true;
 const peers = {}
+
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
@@ -132,3 +133,4 @@ const setPlayVideo = () => {
   `
   document.querySelector('.main__video_button').innerHTML = html;
 }
+
